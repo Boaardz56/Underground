3f5265d5f7e27faf1e4c2213f97c388f76bd1d26
Boaardz56 <jackie.lopez.56@gmail.com>
2020-07-24 13:39:52 -0400

    added handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,11 @@
 DROP SCHEMA IF EXISTS sakila;
 CREATE SCHEMA sakila;
 USE sakila;
+
+CREATE TABLE stores
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	open BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE sakila;
+
+INSERT INTO stores (name, open) VALUES ("Orlando Underground", true);
+INSERT INTO stores (name, open) VALUES ("Miami Underground", false);
+INSERT INTO stores (name, open) VALUES ("Boston Films", true);
